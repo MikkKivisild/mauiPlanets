c09b87ebc3d5cdd3ee75b7091943fbf34d7b6f06
opilane <opilane@HP-CZC0359NN9>
2025-02-05 10:20:40 +0200

    Add project files.

---
+++ mauiPlanets/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace mauiPlanets
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
